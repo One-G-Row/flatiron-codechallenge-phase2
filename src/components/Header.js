8a9204ed5dda81d3be98758362326622bc794d3a
student <kmac.kimaru93@gmail.com>
2024-04-27 19:58:14 +0300

    getting input value from input field

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,20 @@
+import React,{useState} from "react"
 
 function Header(){
+const [input, setInput] = useState()
+
+function handleInput(e){
+setInput(e.target.value)
+}
+
+console.log(input)
+
+return(
+<div>
+<h1>Bank of FlatIron</h1>
+<input type="text" onChange={handleInput}/>
+</div> 
+)
 
 }
 export default Header
